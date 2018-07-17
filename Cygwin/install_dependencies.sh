b2d43735adea88f9220e64749f67fad7a3f7b930
Kevin Mangold <kevin.mangold@nist.gov>
2018-07-17 15:37:30 -0400

    updating install script

--- Cygwin/install_dependencies.sh
+++ Cygwin/install_dependencies.sh
@@ -25,20 +25,22 @@ pip2 install --user flask pymongo xmltodict flask_login flask_wtf docopt flask_m
 # Download and install MongoDB
 #wget http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-latest.zip
 #unzip mongodb-win32-x86_64-2008plus-ssl-latest.zip
-wget http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.7.3.zip
-unzip mongodb-win32-x86_64-2008plus-ssl-3.7.3.zip
-chmod +x mongodb-win32-x86_64-2008plus-ssl-3.7.3/bin/*
-mv mongodb-win32-x86_64-2008plus-ssl-3.7.3/bin/* /usr/local/bin
-mkdir -p /cygdrive/c/data/db
+
+## For Cygwin
+#wget http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.7.3.zip
+#unzip mongodb-win32-x86_64-2008plus-ssl-3.7.3.zip
+#chmod +x mongodb-win32-x86_64-2008plus-ssl-3.7.3/bin/*
+#mv mongodb-win32-x86_64-2008plus-ssl-3.7.3/bin/* /usr/local/bin
+#mkdir -p /cygdrive/c/data/db
 
 #####
 # mongod &
 #####
 
 # Download PPQAS Source from GitHub
-cd ~
-git clone https://github.com/usnistgov/PPQAS.git
+#cd ~
+#git clone https://github.com/usnistgov/PPQAS.git
 
-cd ~/PPQAS/ps-aug19-2017
-python runserver.py
-python runserver.py > runserver_`date +%s`.log 2>&1 &
+#cd ~/PPQAS/ps-aug19-2017
+#python runserver.py
+#python runserver.py > runserver_`date +%s`.log 2>&1 &
